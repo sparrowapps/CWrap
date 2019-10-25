0087e0b8c76aea8798e8489f1b27ad7095d3d249
jhchoi_ex2i <jhchoi@ex2i.com>
2019-10-25 14:34:31 +0900

    Initial Commit

---
+++ CWrap/CWrap.h
@@ -0,0 +1,19 @@
+//
+//  CWrap.h
+//  CWrap
+//
+//  Created by sparrow on 2019/10/25.
+//  Copyright © 2019 SPARROWAPPS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CWrap.
+FOUNDATION_EXPORT double CWrapVersionNumber;
+
+//! Project version string for CWrap.
+FOUNDATION_EXPORT const unsigned char CWrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CWrap/PublicHeader.h>
+
+
